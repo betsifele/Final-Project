f3155794c34fbb6afef164ce6c5a26aeab7cfe24
betsifele <74524669+betsifele@users.noreply.github.com>
2023-03-26 22:38:09 -0400

    Add files via upload

--- FINAL PROJECT - CPS 188.c
+++ FINAL PROJECT - CPS 188.c
@@ -1,45 +1,100 @@
 #include <stdio.h>
 
-
-#define Rows 211
-#define Columns 18
-
+typedef struct{
+	
+	int date;
+	char geo[50];
+	char dguid[50];
+	char agegroup[50];
+	char sex[50];
+	char indicators[50];
+	char characteristics[50];
+	char UOM[50];
+	int UOM_ID;
+	char scalarfactor[50];
+	int scalarID;
+	char vector[50];
+	char coordinate[50];
+	double value;
+	char status;
+	char symbol;
+	char terminate;
+	int decimals;
+}statscan;
+	
+	
+	
 int main(void){
 	
+	FILE *file = fopen("statscan_diabetes1.csv", "r");
 	
-	FILE *csvfile = fopen("statscan_diabetes1.csv","r");
-	char array [Rows][Columns];
-	int row = 0;
+	if(file == NULL){
+		printf("Error opeing the file.\n");
+		return 1;
+	}
 	
-	while(!feof(csvfile)){
-		if(ferror(csvfile)){
-			printf("Error Reading file");
+	statscan stats[211];
+	
+	int read = 0;
+	int records = 0;
+	
+	
+	do{
+		
+	
+		read = fscanf(file,"%d,%49[^,],%49[^,],%49[^,],%49[^,],%49[^,],%49[^,],%49[^,],%d,%49[^,] ,%d,%49[^,],%49[^,],%lf %c %c %c %d\n",
+			                     &stats[records].date,
+                                 stats[records].geo,
+                                 stats[records].dguid,
+	                             stats[records].agegroup,
+	                             stats[records].sex,
+	                             stats[records].indicators,
+	                             stats[records].characteristics,
+	                             stats[records].UOM,
+	                             &stats[records].UOM_ID,
+	                             stats[records].scalarfactor,
+	                             &stats[records].scalarID,
+	                             stats[records].vector,
+	                             stats[records].coordinate,
+	                             &stats[records].value,
+	                             &stats[records].status,
+	                             &stats[records].symbol,
+	                             &stats[records].terminate,
+	                             &stats[records].decimals);
+
+		if(read == 18){
+			records++;
+		}
+		
+		if(read != 18 && !feof(file)){
+			printf("File format incorrect. \n");
 			return 1;
-		}
-		
-		
-		for(int i = 0;i< Columns;i++){
-			if((fscanf(csvfile,"%c", &array[row][i]))== EOF){
-				break;
-				}
 			}
-			
-		row++;
 		
-		if(row == Rows){
-			break;
-			}
-		}
-	fclose(csvfile);
+		if(ferror(file)){
+			printf("Error reading the file.\n");
+			return 1;
+			}		
+		
+	}while(!feof(file));
 	
-	for(int i = 0;i < Rows;i++){
-		for(int j = 0; j < Columns;j++){
-			printf("%c",array[i][j]);
-			if(j == Columns){
-				printf("\n");
-			}
-		}
+	fclose(file);
+	
+	
+	printf("\n%d records read. \n\n", records);
+	
+	
+	printf("|REF_DATE (year) \t\t|\tGeo \t\t| \tAge Group \t\t|\tSex\t\t|\t Value \t\t|");
+	for (int i = 0; i< records; i++){
+		printf("|%d|%s|%s|%s|%.2lf|", stats[i].date,
+		                              stats[i].geo,
+		                              stats[i].agegroup,
+		                              stats[i].sex,
+		                              stats[i].value);
+		                              printf("\n");
+
+		}	
 	
 	return(0);
-}
-}
+	} 
+
